6ae65fcab5548d754b75e0a32f666c90882fde71
Thorbjørn Ravn Andersen <thorbjoern@gmail.com>
2021-12-24 22:35:34 +0100

    Found all initial info in jgit preparing for JAP test.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gaps
+
 Git information As Plain Source
+
+We want to know:
+
+* Current branch name (or sha1 if detached).
+* Last commit
+    * Who (full name GIT_AUTHOR_NAME, email GIT_AUTHOR_EMAIL)
+    * When (GIT_AUTHOR_DATE)
+    * What (message)
+
+(from https://git-scm.com/book/en/v2/Git-Internals-Environment-Variables)
+
+Anything else needs to be on a "are you sure this is needed?" basis.
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -2,6 +2,7 @@ package demo;
 
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
@@ -11,14 +12,17 @@ import java.util.Date;
 
 public class Main {
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         FileRepositoryBuilder builder = new FileRepositoryBuilder();
         //Repository repository = builder.setGitDir(new File("/Users/ravn/git/gaps/src/main/java/demo"))
         Repository repository = builder.setGitDir(new File("/Users/ravn/git/gaps/.git"))
                 .readEnvironment() // scan environment GIT_* variables
                 .findGitDir() // scan up the file system tree
                 .build();
-        System.out.println(repository.getBranch());
+        System.out.println("GIT_BRANCH=" + repository.getBranch());
+        System.out.println(repository.getFullBranch());
+        StoredConfig config = repository.getConfig();
+        System.out.println(config);
         var here = repository.getWorkTree();
         System.out.println(here);
         var head = repository.resolve(Constants.HEAD);
@@ -26,9 +30,13 @@ public class Main {
         var revWalk = new RevWalk(repository);
         RevCommit revCommit = revWalk.parseCommit(head);
         System.out.println(revCommit);
-        System.out.println(new Date(revCommit.getCommitTime() * 1000l));
-        System.out.println(revCommit.getAuthorIdent().getName());
-        System.out.println(revCommit.getFullMessage());
+        System.out.println(new Date(revCommit.getCommitTime() * 1000L));
+        System.out.println(revCommit.getAuthorIdent());
+        System.out.println("GIT_AUTHOR_NAME=" + revCommit.getAuthorIdent().getName()); // protect all non-ascii characters, and quotes.
+        System.out.println("GIT_AUTHOR_DATE=" + revCommit.getAuthorIdent().getWhen()); //.getTime());
+        System.out.println("GIT_AUTHOR_EMAIL=" + revCommit.getAuthorIdent().getEmailAddress());// protect all non-ascii characters, and quotes.
+        System.out.println(revCommit.getAuthorIdent().toExternalString());
+        System.out.println("GIT_MESSAGE=" + revCommit.getFullMessage()); // protect all non-ascii characters, and quotes.
         System.out.println(revCommit.getName());
         System.out.println(revCommit);
         revWalk.dispose();
