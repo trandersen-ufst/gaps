5ff139def5c12902aa2556c2835c1b7b56534d29
Thorbjørn Ravn Andersen <thorbjoern@gmail.com>
2021-12-25 17:31:18 +0100

    processor gets called with "mvn clean package"

--- gaps-processor/src/main/java/gaps/GapsProcessor.java
+++ gaps-processor/src/main/java/gaps/GapsProcessor.java
@@ -2,18 +2,21 @@ package gaps;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.util.Set;
 
-//@SupportedAnnotationTypes("*")
-// @SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes("gaps.Gaps")
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
 public class GapsProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (TypeElement annotation : annotations) {
             for (var element : roundEnv.getElementsAnnotatedWith(annotation)) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "found @Gaps at " + element);
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, ">>> found @Gaps at " + element);
             }
         }
         return true;
