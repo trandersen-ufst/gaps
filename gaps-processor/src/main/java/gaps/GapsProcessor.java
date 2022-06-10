91f5ecc7e4e47ce86a1113d9b5981b62cb006812
Thorbjørn Ravn Andersen - W31473 <Thorbjoern.Andersen@ufst.dk>
2022-06-10 14:42:58 +0200

    #1 Cleaned up code.

--- gaps-processor/src/main/java/gaps/GapsProcessor.java
+++ gaps-processor/src/main/java/gaps/GapsProcessor.java
@@ -18,16 +18,11 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
-import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.Writer;
-import java.lang.reflect.Field;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
@@ -46,8 +41,6 @@ public class GapsProcessor extends AbstractProcessor {
             Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(annotation);
             for (Element element : elements) {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, ">>> found @Gaps at " + element);
-                // FIXME:  Generate source and write to disk.
-
                 try {
                     // The Baeldung tutorial did it this way.
                     String fullClassName = ((TypeElement) element).getQualifiedName().toString();
@@ -67,44 +60,31 @@ public class GapsProcessor extends AbstractProcessor {
 
                     String fullGapsClassName = fullClassName + "Gaps";
                     String gapsClassName = className + "Gaps";
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, ">>> writing " + fullGapsClassName);
 
-                    JavaFileObject builderFile = processingEnv.getFiler().createSourceFile(fullGapsClassName); // JavacFiler$FilerOutputJavaFileObject
+                    // Notify in Maven log.
+                    processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, ">>> writing " + fullGapsClassName);
 
-                    System.err.println("builderFile = " + builderFile + ", " + builderFile.getClass());
+                    JavaFileObject builderFile = processingEnv.getFiler().createSourceFile(fullGapsClassName);
 
-                    // https://stackoverflow.com/q/25192381/18619318 comment
+                    // We need _some_ way to locate the git repository in the file system.
+                    // The annotation processor API hides this very well, so we just hope
+                    // that the current classloader points somewhere inside the repository.
+                    // This was found in https://stackoverflow.com/q/25192381/18619318 comment
+
                     URL resource = this.getClass().getClassLoader().getResource(".");
-                    String resourceFileString = resource.getFile();
 
                     Path somewhereInGitRepository = Paths.get(resource.toURI());
 
-                    System.err.println("resource=" + resource + ", resourceFileString=" + resourceFileString + ", somewhereInGitCheckout=" + somewhereInGitRepository);
-
                     Repository repository = new FileRepositoryBuilder()
-                            .readEnvironment() // scan environment GIT_* variables
                             .findGitDir(somewhereInGitRepository.toFile()) // scan up the file system tree
                             .build();
 
+                    // Found by experimentation.
                     ObjectId head = repository.resolve(Constants.HEAD);
-                    System.err.println("head=" + head);
                     RevWalk revWalk = new RevWalk(repository);
                     RevCommit revCommit = revWalk.parseCommit(head);
-                    System.err.println("revCommit=" + revCommit);
-                    System.err.println("GIT_COMMITTER_DATE=" + new Date(revCommit.getCommitTime() * 1000L));
-                    System.err.println("authorIdent=" + revCommit.getAuthorIdent());
-                    System.err.println("GIT_AUTHOR_NAME=" + revCommit.getAuthorIdent().getName()); // protect all non-ascii characters, and quotes.
-                    System.err.println("GIT_AUTHOR_DATE=" + revCommit.getAuthorIdent().getWhen()); //.getTime());
-                    System.err.println("GIT_AUTHOR_EMAIL=" + revCommit.getAuthorIdent().getEmailAddress());// protect all non-ascii characters, and quotes.
-                    System.err.println(revCommit.getAuthorIdent().toExternalString());
-                    System.err.println("GIT_MESSAGE=" + revCommit.getFullMessage()); // protect all non-ascii characters, and quotes.
 
-                    System.err.println("GitDir=" + repository.getWorkTree());
-                    System.err.println("GIT_BRANCH=" + repository.getBranch());
-
-
-
-                    // https://github.com/square/javapoet
+                    // Generate Java source using https://github.com/square/javapoet
 
                     FieldSpec gitBranchField = FieldSpec.builder(String.class, "GIT_BRANCH")
                             .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
@@ -129,7 +109,7 @@ public class GapsProcessor extends AbstractProcessor {
 
                     FieldSpec gitAuthorEmailField = FieldSpec.builder(String.class, "GIT_AUTHOR_EMAIL")
                             .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                            .initializer("$S", repository.getBranch())
+                            .initializer("$S", revCommit.getAuthorIdent().getEmailAddress())
                             .build();
 
                     FieldSpec gitFullMessageField = FieldSpec.builder(String.class, "GIT_MESSAGE")
@@ -148,16 +128,15 @@ public class GapsProcessor extends AbstractProcessor {
                             .build();
 
                     JavaFile javaFile = JavaFile.builder(packageName, helloWorld)
+                            .addFileComment("$L", "Automatically generated.  Do not edit!")
                             .build();
 
-                    try(Writer builderWriter = builderFile.openWriter()) {
+                    try (var ensureRepositoryClosed = repository;
+                         Writer builderWriter = builderFile.openWriter()) {
                         javaFile.writeTo(builderWriter);
                     }
-                    try(var ensureRepositoryClosed = repository) {
-                        // --
-                    }
                 } catch (IOException | URISyntaxException e) {
-                    throw new RuntimeException(e);
+                    throw new RuntimeException(e); // just fail the build.
                 }
             }
         }
